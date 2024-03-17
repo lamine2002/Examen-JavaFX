ec18ef7ae37514e2f2a4fc0cdae580e6f3dcd319
Niang Mohamed Lamine <106320478+lamine2002@users.noreply.github.com>
2024-03-17 00:46:14 +0000

    implementation de la classe Category

--- src/main/java/com/example/examen/lib/Product.java
+++ src/main/java/com/example/examen/lib/Product.java
@@ -1,4 +1,68 @@
 package com.example.examen.lib;
 
 public class Product {
+    private int id;
+    private String libelle;
+    private int quantite;
+    private double prixUnitaire;
+    private String categorie;
+
+    public Product(int id, String libelle, int quantite, double prixUnitaire, String categorie) {
+        this.id = id;
+        this.libelle = libelle;
+        this.quantite = quantite;
+        this.prixUnitaire = prixUnitaire;
+        this.categorie = categorie;
+    }
+
+    public Product(String libelle, int quantite, double prixUnitaire, String categorie) {
+        this.libelle = libelle;
+        this.quantite = quantite;
+        this.prixUnitaire = prixUnitaire;
+        this.categorie = categorie;
+    }
+
+    public Product() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public int getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(int quantite) {
+        this.quantite = quantite;
+    }
+
+    public double getPrixUnitaire() {
+        return prixUnitaire;
+    }
+
+    public void setPrixUnitaire(double prixUnitaire) {
+        this.prixUnitaire = prixUnitaire;
+    }
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(String categorie) {
+        this.categorie = categorie;
+    }
+
 }
